00984c69c3cac4c9d057b317d96ae312c775be7e
Joanne Chen <joanne_r_chen@yahoo.com>
2021-01-16 21:48:04 +0000

    database connect and recipe route set up finished

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+
+//Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running!!'));
+
+// Definte Routes
+app.use('/api/recipes', require('./routes/api/recipes'));
 
 const PORT = process.env.PORT || 5000;
 
