2130e479544bff4574f430214ddb879b425cca93
Joanne Chen <joanne_r_chen@yahoo.com>
2021-02-24 22:00:42 +0800

    date format is permanent in Diary.js

--- client/src/components/Diary.js
+++ client/src/components/Diary.js
@@ -4,26 +4,39 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import moment from 'moment';
 
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 
 export const Diary = () => {
-    const [diary, setDiary] = useState([])
+    const [diary, setDiary] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+
+    const formatter = (eachEvent) => {
+        const x = moment(eachEvent.date).format('DD/MM/YYYY');
+        return eachEvent.date = x;
+    }
+
+    const changeDate = async (diaryData) => {
+        diaryData.forEach(each => formatter(each));
+        setLoading(false);    
+    }
 
     useEffect(() => {
+        
         const fetchDiary = async () => {
-
             const res = await axios.get('/api/diary')
-
+            console.log(res.data)
+            changeDate(res.data);
             setDiary(res.data)
-            console.log(diary)
         }
+        
         fetchDiary();
     }, [])
-
-    const dateFormatter = (jsonDate) => {
-        return moment(jsonDate).format('DD/MM/YYYY')
-    }
+    
+    
+    
+    
 
     return (
         <div>
@@ -39,17 +52,23 @@ export const Diary = () => {
             <br></br>
             <h1>My Daily Activities</h1>
             <br></br>
-            <div style={{display: 'inline-block'}}>
-                <LineChart width={500} height={400} data={diary} >
-                    <Line type="monotone" dataKey="physicalScore" stroke="#5F9EA0"/>
-                    <Line type="monotone" dataKey="musicScore" stroke="#B22222"/>
-                    <Line type="monotone" dataKey="workScore" stroke="#FFD700"/>
-                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
-                    <XAxis dataKey="date" tickFormatter={dateFormatter} />
-                    <YAxis />
-                    <Tooltip tickFormatter={dateFormatter}/>
-                </LineChart>
-            </div>
+            {loading === false ? (
+
+                <div style={{display: 'inline-block'}}>
+                    <LineChart width={850} height={400} data={diary} >
+                        <Line type="monotone" dataKey="physicalScore" stroke="#5F9EA0"/>
+                        <Line type="monotone" dataKey="musicScore" stroke="#B22222"/>
+                        <Line type="monotone" dataKey="workScore" stroke="#FFD700"/>
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+                        <XAxis dataKey="date" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend verticalAlign="top" height={36} />
+                    </LineChart>
+                </div>
+            ) : (
+                <p>loading....</p>
+            )}
         </div>
     )
 }
--- client/src/components/layout/DiaryForm.js
+++ client/src/components/layout/DiaryForm.js
@@ -64,12 +64,9 @@ const DiaryForm = () => {
             console.error(err.response.data);
         }
 
-
-
-
     }
     return (
-        <div style={{padding: '15rem'}}>
+        <div style={{padding: '10rem'}}>
             <Form onSubmit={e => onSubmit(e)} >
                 <div style={{display: 'inline-block'}}>
                     <Calendar 
@@ -79,86 +76,89 @@ const DiaryForm = () => {
                 </div>
                 <br></br>
                 <br></br>
-                <Form.Group controlId="yoga" onChange={e => setYoga(e.target.value)}>
-                    <Form.Label style={{color: '#008000'}}>Yoga</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="0">none</option>
-                        <option value="30">30min</option>
-                        <option value="60">60min</option>
-                    </Form.Control>
-                </Form.Group>
+                <div style={{marginLeft: '32.5%'}}>
 
-                <Form.Group controlId="walk" onChange={e => setWalk(e.target.value)}>
-                    <Form.Label style={{color: '#008000'}}>Walking</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="0">none</option>
-                        <option value="30">30min</option>
-                        <option value="60">60 - 90min</option>
-                        <option value="120">120+ min</option>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="yoga" onChange={e => setYoga(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: '#008000'}}>Yoga</Form.Label>
+                        <Form.Control as="select" required>
+                            <option>Please select</option>
+                            <option value="0">none</option>
+                            <option value="30">30min</option>
+                            <option value="60">60min</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="run" onChange={e => setRun(e.target.value)}>
-                    <Form.Label style={{color: '#008000'}}>Running</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="0">none</option>
-                        <option value="20">20min</option>
-                        <option value="30">30min</option>
-                    </Form.Control>
-                </Form.Group>
+                        <Form.Group controlId="walk" onChange={e => setWalk(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: '#008000'}}>Walking</Form.Label>
+                        <Form.Control as="select">
+                            <option>Please select</option>
+                            <option value="0">none</option>
+                            <option value="30">30min</option>
+                            <option value="60">60 - 90min</option>
+                            <option value="120">120+ min</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="video" onChange={e => setVideo(e.target.value)}>
-                    <Form.Label style={{color: '#008000'}}>Exercise video</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="0">none</option>
-                        <option value="20">20 min</option>
-                        <option value="30">30min</option>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="run" onChange={e => setRun(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: '#008000'}}>Running</Form.Label>
+                        <Form.Control as="select">
+                            <option>Please select</option>
+                            <option value="0">none</option>
+                            <option value="20">20min</option>
+                            <option value="30">30min</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="violin" onChange={e => setViolin(e.target.value)}>
-                    <Form.Label style={{color: 'red'}}>Violin Practice</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="0">none</option>
-                        <option value="90">60 - 90min</option>
-                        <option value="120">120+ min</option>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="video" onChange={e => setVideo(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: '#008000'}}>Exercise video</Form.Label>
+                        <Form.Control as="select">
+                            <option>Please select</option>
+                            <option value="0">none</option>
+                            <option value="20">20 min</option>
+                            <option value="30">30min</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="music" onChange={e => setMusic(e.target.value)}>
-                    <Form.Label style={{color: 'red'}}>Concert or Rehearsal</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="0">none</option>
-                        <option value="90">60 - 90min</option>
-                        <option value="120">120+ min</option>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="violin" onChange={e => setViolin(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: 'red'}}>Violin Practice</Form.Label>
+                        <Form.Control as="select">
+                            <option>Please select</option>
+                            <option value="0">none</option>
+                            <option value="90">60 - 90min</option>
+                            <option value="120">120+ min</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="coding" onChange={e => setCoding(e.target.value)}>
-                    <Form.Label style={{color: 'gold'}}>Coding</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="null">none</option>
-                        <option value="half">Half Day</option>
-                        <option value="full">Full Day</option>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="music" onChange={e => setMusic(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: 'red'}}>Concert or Rehearsal</Form.Label>
+                        <Form.Control as="select">
+                            <option>Please select</option>
+                            <option value="0">none</option>
+                            <option value="90">60 - 90min</option>
+                            <option value="120">120+ min</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="teaching" onChange={e => setTeaching(e.target.value)}>
-                    <Form.Label style={{color: 'gold'}}>Teaching</Form.Label>
-                    <Form.Control as="select">
-                        <option>Please select</option>
-                        <option value="null">none</option>
-                        <option value="small">1 - 2 students</option>
-                        <option value="large">Full Day</option>
-                    </Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="coding" onChange={e => setCoding(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: 'gold'}}>Coding</Form.Label>
+                        <Form.Control as="select">
+                            <option>Please select</option>
+                            <option value="null">none</option>
+                            <option value="half">Half Day</option>
+                            <option value="full">Full Day</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId="teaching" onChange={e => setTeaching(e.target.value)} style={{width: '50%'}}>
+                        <Form.Label style={{color: 'gold'}}>Teaching</Form.Label>
+                        <Form.Control as="select">
+                            <option>Please select</option>
+                            <option value="null">none</option>
+                            <option value="small">1 - 2 students</option>
+                            <option value="large">Full Day</option>
+                        </Form.Control>
+                    </Form.Group>
+                </div>
                 <Button variant="danger" type="submit">
                     Submit
                 </Button>
